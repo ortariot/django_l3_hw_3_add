ef5e987e925206d3cca8751293139aea622e014a
ortariot <ortariot@gmail.com>
2021-09-21 17:39:48 +0700

    part 1 completed

--- books/views.py
+++ books/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from books.models import Book
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books_query = Book.objects.all()
+    context = {'books' : books_query}
     return render(request, template, context)
