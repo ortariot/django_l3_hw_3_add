43cca17623ceb3420a93b00e8f044c3595ae2625
ortariot <ortariot@gmail.com>
2021-09-22 18:27:41 +0700

    all cases successfully completed

--- books/views.py
+++ books/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from books.models import Book
-from django.core.paginator import Paginator
 
 
 def books_view(request):
@@ -11,20 +10,17 @@ def books_view(request):
 
 
 def books_of_date_view(request, date):
-    books_query = Book.objects.order_by('pub_date')
-    print(len(books_query))
-    paginate = Paginator(books_query, len(books_query.filter(pub_date=date)))
-
-    data_list = paginate.get_page(2)
-    books_query = Book.objects.filter(pub_date=date)
-    # print(books_query)
-    prev_page = data_list.previous_page_number if data_list.has_previous() else None
-    next_page = data_list.next_page_number if data_list.has_next() else None
-
-
     template = 'books/books_list.html'
+    books_query = Book.objects.order_by('pub_date')
 
-    context = {'books': data_list,
+    books_query = Book.objects.filter(pub_date=date)
+    prev = Book.objects.filter(pub_date__lt=date).order_by('pub_date')
+    next = Book.objects.filter(pub_date__gt=date).order_by('pub_date')
+    prev_page = prev[prev.count() - 1].pub_date.strftime("%Y-%m-%d")\
+        if prev.count() > 0 else None
+    next_page = next[0].pub_date.strftime("%Y-%m-%d")\
+        if next.count() > 0 else None
+    context = {'books': books_query,
                'prev_page_url': prev_page,
                'next_page_url': next_page,
                }
